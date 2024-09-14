285a067f8bcd60bc8d1add80d36af9ce37ad1448
Animesh Shukla <animeshshukla1518@gmail.com>
2024-09-14 11:03:29 +0530

    feat: added moves

--- src/gameManager/gameManager.ts
+++ src/gameManager/gameManager.ts
@@ -1,19 +1,35 @@
 import { Chess } from 'chess.js'
 
 export class GameManager{
-    blackPlayerUserID:String;
-    whitePlayerUserID:String;
+    blackPlayerUserID:string;
+    whitePlayerUserID:string;
     result:Result;
     game:Chess;
 
-    constructor(blackPlayerUserID:String, whitePlayerUserID:String){
+    constructor(blackPlayerUserID:string, whitePlayerUserID:string){
         this.blackPlayerUserID = blackPlayerUserID;
         this.whitePlayerUserID = whitePlayerUserID;
         this.game = new Chess();
         this.result = Result.IN_PROGRESS;
+    }  
+    
+    getDetails(){
+        console.log("Black Player: ",this.blackPlayerUserID);
+        console.log("White Player: ",this.whitePlayerUserID);
+        console.log("Result: ",this.result.toString());
+        console.log("Game: ",this.game.ascii());
+    }
 
-        console.log(this.game.ascii());
-    }   
+    move(userID:string, move:string){
+        console.log(this.blackPlayerUserID);
+        if(this.blackPlayerUserID===userID && this.game.turn()==='b'){
+            this.game.move(move);
+        }else if(this.whitePlayerUserID===userID && this.game.turn()==='w'){
+            this.game.move(move);
+        }else{
+            console.log("Invalid Move");
+        }
+    }
 }
 
 export enum Result{
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import WebSocket, {WebSocketServer} from 'ws';
 import http from 'http';
+import {uuid} from 'uuidv4';
 import { GameManager } from './gameManager/gameManager';
 
 const server = http.createServer(function(request:any,response:any){
@@ -9,36 +10,46 @@ const server = http.createServer(function(request:any,response:any){
 
 const wss = new WebSocketServer({server});
 
-let userCount=0;
+let users=[];
+let gameManager:GameManager | null = null;
 
 wss.on('connection',function connection(socket){
-    if(userCount==2){
+    if(users.length==2){
         socket.send("Sorry, the game is full");
         socket.close();
         return;
     }
     socket.on('error',console.error);
 
-    socket.on('message',function message(data,binary){
-        console.log("message recieved");
-        wss.clients.forEach(function each(client){
-            if(client.readyState===WebSocket.OPEN){
-                client.send(data,{binary});
-            }
-        });
+    socket.on('message', function message(data, binary) {
+        try {
+            const jsonString = data.toString();
+            const jsonData = JSON.parse(jsonString);
+            gameManager?.move(jsonData.userID, jsonData.move);
+            gameManager?.getDetails();
+        } catch (error) {
+            console.error("Error parsing JSON: ", error);
+        }
     });
     
-    console.log("Users connected ",++userCount);
 
-    if(userCount==2){
+    users.push(uuid());
+    
+    console.log("Users connected ",users.length);
+
+    if(users.length==2){
+        let i=0;
         wss.clients.forEach(function each(client){
             if(client.readyState===WebSocket.OPEN){
-                client.send("Game is ready to start");
+                client.send("Game is ready to start"+users[i]);
+                i++;
             }
         });
 
-        const gameManager = new GameManager("black","white");
+        gameManager = new GameManager(users[0], users[1]);
+        gameManager.getDetails();
     }
+
     socket.send("Hello from the server");
 });
 
